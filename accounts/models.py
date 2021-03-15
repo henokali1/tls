fa7e49055dc77bd49800078267be93ecfe8286b6
Ubuntu <ubuntu@ip-172-31-43-226.us-east-2.compute.internal>
2021-03-15 07:28:35 +0000

    Department and Job title in accounts

--- accounts/models.py
+++ accounts/models.py
@@ -6,3 +6,17 @@ class CustomUser(AbstractUser):
 
 	def __str__(self):
 		return self.email
+
+# All Departments
+class Department(models.Model):
+	department = models.CharField(max_length=100, default='')
+
+	def __str__(self):
+		return self.department
+
+# All Job Titles
+class JobTitle(models.Model):
+	job_title = models.CharField(max_length=100, default='')
+
+	def __str__(self):
+		return self.job_title
